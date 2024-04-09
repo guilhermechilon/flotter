d986783db4be186724b34b1cad3168454537d0c2
Yan Lucas <yanlucas2502@gmail.com>
2024-04-09 12:07:53 -0300

    Cadastro de Rota - Yan

--- src/pages/CadastroRotas/index.js
+++ src/pages/CadastroRotas/index.js
@@ -1,9 +1,60 @@
 import React from "react";
+import {
+  Imagem,
+  InputField,
+  InputWrapper,
+  Subtitle,
+  Title,
+  Wrapper,
+  Form,
+  DefaultButton,
+} from "./styles";
+import { FaUserCircle } from "react-icons/fa";
+import { FaCalendarAlt } from "react-icons/fa";
+import { MdOutlineSpeed } from "react-icons/md";
+import { FaIdCard } from "react-icons/fa";
+import { IoLocationSharp } from "react-icons/io5";
 
-import { Wrapper } from "./styles";
-
-function Cadastro() {
-  return <Wrapper>asd</Wrapper>;
+function RegistroMaquina() {
+  return (
+    <>
+      <Wrapper>
+        <Title>CADASTRO DE ROTA</Title>
+        <Subtitle>
+          <hr style={{ width: "100%", borderTop: "solid 1px black" }} />
+        </Subtitle>
+        <Form>
+          <InputWrapper>
+            <FaUserCircle size={30} />
+            <InputField placeholder="Destino" />
+          </InputWrapper>
+          <InputWrapper>
+            <FaCalendarAlt size={30} />
+            <InputField placeholder="Horário de Saída" />
+          </InputWrapper>
+          <InputWrapper>
+            <IoLocationSharp size={30} />
+            <InputField placeholder="Origem" />
+          </InputWrapper>
+          <InputWrapper>
+            <FaIdCard size={30} />
+            <InputField placeholder="Placa do veículo" />
+          </InputWrapper>
+          <InputWrapper>
+            <MdOutlineSpeed size={30} />
+            <InputField placeholder="Quilometragem" />
+          </InputWrapper>
+          <DefaultButton
+            primaryColor
+            width={"350px"}
+            onClick={() => alert("Vai ter um pop-up aqui um dia")}
+          >
+            Cadastrar Rota
+          </DefaultButton>
+        </Form>
+      </Wrapper>
+    </>
+  );
 }
 
-export default Cadastro;
+export default RegistroMaquina;
--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import React from "react";
-import Cadastro from "./pages/CadastroRotas";
 import ErrorPage from "./pages/ErrorPage";
-import RegistroMaquina from "./pages/RegistroMaquina";
+import CadastroRotas from "./pages/CadastroRotas";
 
 export default function Rotas() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Cadastro />}></Route>
-          <Route path="/cadastro" element={<Cadastro />}></Route>
-          <Route path="/registromaquina" element={<RegistroMaquina />}></Route>
+          <Route path="/cadastrorotas" element={<CadastroRotas />}></Route>
           <Route path="*" element={<ErrorPage />}></Route>
         </Routes>
       </BrowserRouter>
